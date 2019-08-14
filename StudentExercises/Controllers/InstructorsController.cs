434c15e9f0823976d737f873060b11ba98b86932
Kristen Norris <krnorris65@gmail.com>
2019-08-14 14:49:42 -0500

    implemented delete and edit functionality

--- StudentExercises/Controllers/InstructorsController.cs
+++ StudentExercises/Controllers/InstructorsController.cs
@@ -127,21 +127,49 @@ namespace StudentExercises.Controllers
         public ActionResult Edit(int id)
         {
             //use GetSingleInstructor to get the Instructor you want to edit
+            Instructor instructor = GetSingleInstructor(id);
             //Use GetAllCohorts to get a list of cohorts
+            List<Cohort> cohorts = GetAllCohorts();
             //pass both the Instructor and the List of Cohorts into an instance of the InstructorEditViewModel
+            var viewModel = new InstructorEditViewModel(instructor, cohorts);
             //pass the instance of the viewModel into View()
-            return View();
+            return View(viewModel);
         }
 
         // POST: Instructors/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, InstructorEditViewModel model)
         {
             try
             {
                 // TODO: Add update logic here
-                return RedirectToAction(nameof(Index));
+                using(SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using(SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Instructor
+                                            SET
+                                                FirstName = @firstName,
+                                                LastName = @lastName,
+                                                SlackHandle = @slackHandle,
+                                                Specialty = @specialty,
+                                                CohortId = @cohortId
+                                            WHERE Id = @id";
+                        cmd.Parameters.AddWithValue("@firstName", model.Instructor.FirstName);
+                        cmd.Parameters.AddWithValue("@lastName", model.Instructor.LastName);
+                        cmd.Parameters.AddWithValue("@slackHandle", model.Instructor.SlackHandle);
+                        cmd.Parameters.AddWithValue("@specialty", model.Instructor.Specialty);
+                        cmd.Parameters.AddWithValue("@cohortId", model.Instructor.CohortId);
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        cmd.ExecuteNonQuery();
+
+                        return RedirectToAction(nameof(Index));
+
+                    }
+                }
             }
             catch
             {
@@ -153,19 +181,33 @@ namespace StudentExercises.Controllers
         public ActionResult Delete(int id)
         {
             //use GetSingleInstructor to get the Instructor you want to delete
+            Instructor instructor = GetSingleInstructor(id);
             //pass that instructor into View()
-            return View();
+            return View(instructor);
         }
 
         // POST: Instructors/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteInstructor(int id)
         {
             try
             {
                 // TODO: Add delete logic here
+                using(SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using(SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM StudentExercise
+                                                WHERE InstructorId = @id;
+                                            DELETE FROM Instructor
+                                                WHERE Id = @id";
+                        cmd.Parameters.AddWithValue("@id", id);
 
+                        cmd.ExecuteNonQuery();
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
---
+++ StudentExercises/Models/ViewModels/InstructorEditViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercises.Models.ViewModels
+{
+    public class InstructorEditViewModel
+    {
+        public Instructor Instructor { get; set; }
+
+        public List<SelectListItem> Cohorts { get; set; }
+
+        public InstructorEditViewModel() { }
+
+        public InstructorEditViewModel(Instructor instructor, List<Cohort> cohortList)
+        {
+            Instructor = instructor;
+            Cohorts = cohortList
+                .Select(cohort => new SelectListItem
+                {
+                    Text = cohort.Name,
+                    Value = cohort.Id.ToString()
+                })
+                .ToList();
+
+            Cohorts.Insert(0, new SelectListItem
+            {
+                Text = "Choose cohort...",
+                Value = "0"
+            });
+        }
+    }
+}
